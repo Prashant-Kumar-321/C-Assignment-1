706f1b7baa9b391a9a72366e2f6bd14baff5b36b
Prashant Kumar Gupta <114841936+Prashant-Kumar-321@users.noreply.github.com>
2022-10-14 21:57:27 +0530

    Update Question _10 11_.c

--- Question _10 11_.c
+++ Question _10 11_.c
@@ -6,9 +6,9 @@ int main()
     // Question 10
 
     int DD, MM, YYY;
-    printf("Enter Today's Date in below formate\nDD/MM/YYYY = ");
+    printf("Enter Date of Birth in given formate\nDD/MM/YYYY = ");
     // Displaying Text message to let the user know in what formate date has to be entered
-    scanf("%d %d %d", &DD, &MM, &YYY);
+    scanf("%d/%d/%d", &DD, &MM, &YYY);
     // taking input from user and storing in defined variable
     printf("\nDay-%d, Month-%d, Year-%d \n\n\n", DD, MM, YYY);
     printf("\a");
@@ -18,7 +18,7 @@ int main()
 
     printf("\nEnter Timing of your Class \nHH:MM = ");
     int HH, mm;
-    scanf("%d %d", &HH, &mm);
+    scanf("%d:%d", &HH, &mm);
     printf("\a n\n%d hours and %d Minutes \n", HH, mm);
 
     getch();
